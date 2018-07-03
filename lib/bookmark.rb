c5d41ad2b501020181a9cfcf56aaa20b122415aa
John Forster <j_forster@hotmail.co.uk>
2018-07-03 11:24:17 +0100

    Connected app to database using pg.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,14 @@
+require 'pg'
+
 class Bookmark
   @bookmarks = ['wikipedia.org', 'facebook.com']
 
   def self.all
-    @bookmarks
+    results = connection.exec 'SELECT url FROM bookmarks'
+    results.map { |bookmark| bookmark['url'] }
+  end
+
+  def self.connection
+    PG.connect dbname: 'bookmark_manager', user: 'johnforster'
   end
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,6 +2,6 @@ feature 'viewing links' do
   scenario 'see all the bookmarks currently available' do
     visit('/')
     click_button("View 'marks")
-    expect(page).to have_content('wikipedia.org')
+    expect(page).to have_content('makersacademy.com')
   end
 end
